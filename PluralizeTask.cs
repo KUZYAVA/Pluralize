0f64aebeec0678cf24e17f10ef401ea70b0dfafe
KUZYAVA <alexchampion2003@gmail.com>
2021-10-11 21:28:56 +0500

    Initial commit: Share all files for education

---
+++ PluralizeTask.cs
@@ -0,0 +1,14 @@
+﻿namespace Pluralize
+{
+    public static class PluralizeTask
+    {
+        public static string PluralizeRubles(int count)
+        {
+            // проверяет, заканчивается число на 11-14 либо на 0 либо на 5-9
+            if (count % 100 > 10 && count % 100 < 15 || count % 10 == 0 || count % 10 >= 5)
+                return "рублей";
+            // проверяет, заканчивается число на 1 или заканчивается на 2-4
+            return count % 10 == 1 ? "рубль" : "рубля";
+        }
+    }
+}
